610e4b17caa3e2af46e5db671003f64abe2d4fb1
yunchun <yunchun@tist.com.tw>
2020-03-14 14:59:57 +0800

    commit

--- src/main/java/com/yunchun/controller/HelloController.java
+++ src/main/java/com/yunchun/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
     @GetMapping("/hello")
     public String hello (Model model, @RequestParam("name") String name) {
+
         model.addAttribute("name", name);
         return "hello";
     }
